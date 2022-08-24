7c745e20b2a6d1f0d1aed689d250a5222961fdcd
Gega <latariagega1@gmail.com>
2022-08-24 19:54:17 +0400

    implemented filter by job position

--- pages/employee/index.js
+++ pages/employee/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useState } from "react";
 import styles from "../../styles/Employees.module.css";
 
 export const getStaticProps = async () => {
@@ -13,6 +14,8 @@ export const getStaticProps = async () => {
 };
 
 export default function EmployeesList({ employees }) {
+  const [selectedPosition, setSelectedPosition] = useState("noposition");
+  const [filteredPosition, setFilteredPosition] = useState(employees);
   let topThree = [];
   employees.sort((a, b) =>
     a.liked > b.liked ? -1 : b.liked > a.liked ? 1 : 0
@@ -22,6 +25,22 @@ export default function EmployeesList({ employees }) {
   });
   topThree = topThree.slice(0, 3);
 
+  const handleChange = (event) => {
+    setSelectedPosition(event.target.value);
+    // if (selectedPosition !== "noposition") {
+    //   let x = employees.filter((a) => {
+    //     if (a.description === selectedPosition) {
+    //       return a;
+    //     }
+    //   });
+    //   setFilteredPosition(x);
+    // } else {
+    //   setFilteredPosition(employees);
+    // }
+  };
+
+  // console.log(filteredPosition);
+
   return (
     <div>
       <h1 className={styles.header}>
@@ -29,36 +48,51 @@ export default function EmployeesList({ employees }) {
       </h1>
       <div>
         <label>
-          Filter by:
+          Filter by position:
+          <select onChange={handleChange} value={selectedPosition}>
+            <option value="noposition">None</option>
+            <option value="Frontend developer">Frontend</option>
+            <option value="Backend developer">Backend</option>
+            <option value="Architect">Architect</option>
+          </select>
+        </label>
+        <h1>{selectedPosition}</h1>
+        <label>
+          Filter by Location:
           <select>
-            <option value="position">Position</option>
-            <option value="location">Location</option>
+            <option value="noloc">None</option>
+            <option value="usa">USA</option>
+            <option value="eur">EUR</option>
+            <option value="geo">GEO</option>
           </select>
         </label>
       </div>
-      {employees.map((employee) => (
-        <Link href={`/employee/${employee.id}`} key={employee.id}>
-          <div
-            className={[
-              `${styles.listitem} ${
-                topThree.includes(employee.name) ? styles.top : ""
-              }`,
-            ]}
-          >
-            <h3>
-              <img
-                className={styles.test}
-                src={`https://test-task-api-optimo.herokuapp.com${employee.avatar}`}
-                alt="avatar"
-                width={"40px"}
-              ></img>
-              {employee.name}
-            </h3>
-            <h3>Position: {employee.description}</h3>
-            <h3>Likes: {employee.liked}</h3>
-          </div>
-        </Link>
-      ))}
+      {employees.map((employee) =>
+        employee.description === selectedPosition ||
+        selectedPosition === "noposition" ? (
+          <Link href={`/employee/${employee.id}`} key={employee.id}>
+            <div
+              className={[
+                `${styles.listitem} ${
+                  topThree.includes(employee.name) ? styles.top : ""
+                }`,
+              ]}
+            >
+              <h3>
+                <img
+                  className={styles.test}
+                  src={`https://test-task-api-optimo.herokuapp.com${employee.avatar}`}
+                  alt="avatar"
+                  width={"40px"}
+                ></img>
+                {employee.name}
+              </h3>
+              <h3>Position: {employee.description}</h3>
+              <h3>Likes: {employee.liked}</h3>
+            </div>
+          </Link>
+        ) : null
+      )}
     </div>
   );
 }
